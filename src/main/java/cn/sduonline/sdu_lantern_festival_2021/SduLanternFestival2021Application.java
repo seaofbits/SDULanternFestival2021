eadff09357f70c2bb5f1a1d2b277647519a77a22
Yzq <1401094334@qq.com>
2021-01-19 21:21:18 +0800

    初始化Spring Boot项目
    Spring Boot初始化时暂时勾选了Spring Web，JDBC和MySQL Driver这3个模块。

---
+++ src/main/java/cn/sduonline/sdu_lantern_festival_2021/SduLanternFestival2021Application.java
@@ -0,0 +1,13 @@
+package cn.sduonline.sdu_lantern_festival_2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SduLanternFestival2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SduLanternFestival2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sduonline/sdu_lantern_festival_2021/SduLanternFestival2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sduonline.sdu_lantern_festival_2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SduLanternFestival2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
