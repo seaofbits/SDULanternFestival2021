2685b10232c2942a6c5a67c7723f78e9f9d2c181
Yzq <1401094334@qq.com>
2021-02-03 09:52:27 +0800

    引入了网络上的RedisConfig和RedisUtil

--- src/main/java/cn/sduonline/sdu_lantern_festival_2021/restcontroller/room/RoomCreateController.java
+++ src/main/java/cn/sduonline/sdu_lantern_festival_2021/restcontroller/room/RoomCreateController.java
@@ -3,22 +3,22 @@ package cn.sduonline.sdu_lantern_festival_2021.restcontroller.room;
 import cn.sduonline.sdu_lantern_festival_2021.entity.Response;
 import cn.sduonline.sdu_lantern_festival_2021.entity.ResponseCode;
 import cn.sduonline.sdu_lantern_festival_2021.restcontroller.login_inspector.Auth;
+import cn.sduonline.sdu_lantern_festival_2021.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @ResponseBody
 public class RoomCreateController {
     @Autowired
-    StringRedisTemplate stringRedisTemplate;
+    RedisUtil redisUtil;
 
     @Auth
     @PostMapping(path = "/room/create")
@@ -28,21 +28,20 @@ public class RoomCreateController {
             return Response.fail(ResponseCode.ERROR_PARAMETER_INVALID_VALUE);
 
         // 创建一个房间（房间id自增）
-        long room_id = stringRedisTemplate.opsForValue().increment("room_id");
+        long roomID = redisUtil.incr("room_id_incr", 1);
 
-        String key = "room-" + room_id;
-        stringRedisTemplate.opsForHash().put(key, "room_id", String.valueOf(room_id));
-        stringRedisTemplate.opsForHash().put(key, "capacity", String.valueOf(capacity));
-        stringRedisTemplate.opsForHash().put(key, "user1_id", String.valueOf(userID));
-        stringRedisTemplate.opsForHash().put(key, "user2_id", String.valueOf(-1));
-        String state;
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("room_id", roomID);
+        attributes.put("capacity", capacity);
+        attributes.put("user1_id", userID);
+        attributes.put("user2_id", -1);
         if (capacity == 1) {
-            state = "full";
+            attributes.put("state", "full");
         } else {
-            state = "unfull";
+            attributes.put("state", "unfull");
         }
-        stringRedisTemplate.opsForHash().put(key, "state", state);
+        redisUtil.hmset("room-" + roomID, attributes);
 
-        return Response.success(Map.of("room_id", room_id));
+        return Response.success(Map.of("room_id", roomID));
     }
 }
