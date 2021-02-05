d154ec24e51f9cd9d20add1b7a2bfde0b5dd2774
Yzq <1401094334@qq.com>
2021-02-05 14:07:20 +0800

    实现了/room/get_attribute接口
    初步进行了“单元测试”，后续需要模拟整个pk或者答题流程进行“集成测试”。

---
+++ src/main/java/cn/sduonline/sdu_lantern_festival_2021/restcontroller/room/RoomGetAttributeController.java
@@ -0,0 +1,38 @@
+package cn.sduonline.sdu_lantern_festival_2021.restcontroller.room;
+
+import cn.sduonline.sdu_lantern_festival_2021.entity.Response;
+import cn.sduonline.sdu_lantern_festival_2021.entity.ResponseCode;
+import cn.sduonline.sdu_lantern_festival_2021.restcontroller.login_inspector.Auth;
+import cn.sduonline.sdu_lantern_festival_2021.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+@ResponseBody
+public class RoomGetAttributeController {
+    @Autowired
+    RedisUtil redisUtil;
+
+    @Auth
+    @PostMapping(path = "/room/get_attribute")
+    public Response getAttr(@RequestParam("room_id") int roomID,
+                            @RequestAttribute("user_id") long userID) {
+        Map attr = redisUtil.hmget("room-" + roomID);
+        // 异常情况
+        if (attr == null) {
+            return Response.fail(ResponseCode.ROOM_INVALID_ID);
+        }
+        if (!attr.get("user1_id").equals(userID)
+                && !attr.get("user2_id").equals(userID)) {
+            return Response.fail(ResponseCode.ROOM_INVALID_USER);
+        }
+
+        return Response.success(attr);
+    }
+}
