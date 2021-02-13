20703c79fa968e320947b12ac40327bda48d23c1
Yzq <1401094334@qq.com>
2021-02-13 11:03:20 +0800

    把@EnableAsync和@EnableScheduling加在了启动类上

--- src/main/java/cn/sduonline/sdu_lantern_festival_2021/SduLanternFestival2021Application.java
+++ src/main/java/cn/sduonline/sdu_lantern_festival_2021/SduLanternFestival2021Application.java
@@ -2,9 +2,13 @@ package cn.sduonline.sdu_lantern_festival_2021;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class SduLanternFestival2021Application {
 
--- src/main/java/cn/sduonline/sdu_lantern_festival_2021/config/SpringScheduleConfig.java
+++
@@ -1,9 +0,0 @@
-package cn.sduonline.sdu_lantern_festival_2021.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SpringScheduleConfig {
-}
